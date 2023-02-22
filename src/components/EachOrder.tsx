1752b82e1edf5c93c8f0f83c4f4659ebdf9b303f
ELLAERA <qwer2407@gmail.com>
2023-02-22 18:43:58 +0900

    added NaverMapView

--- src/components/EachOrder.tsx
+++ src/components/EachOrder.tsx
@@ -1,4 +1,11 @@
-import {Alert, Pressable, StyleSheet, Text, View} from 'react-native';
+import {
+  Alert,
+  Dimensions,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import React, {useCallback, useState} from 'react';
 import orderSlice, {Order} from '../slices/order';
 import {useAppDispatch} from '../store';
@@ -8,6 +15,7 @@ import Config from 'react-native-config';
 import {RootState} from '../store/reducer';
 import {NavigationProp, useNavigation} from '@react-navigation/native';
 import {LoggedInParamList} from '../../AppInner';
+import NaverMapView, {Marker, Path} from 'react-native-nmap';
 
 function EachOrder({item}: {item: Order}) {
   const dispatch = useAppDispatch();
@@ -15,6 +23,7 @@ function EachOrder({item}: {item: Order}) {
   const [loading, setLoading] = useState(false);
   const [detail, setDetail] = useState(false);
   const accessToken = useSelector((state: RootState) => state.user.accessToken);
+  const {start, end} = item;
   const toggleDetail = useCallback(() => {
     setDetail(prev => !prev);
   }, []);
@@ -59,8 +68,41 @@ function EachOrder({item}: {item: Order}) {
       </Pressable>
       {detail ? (
         <View>
-          <View>
-            <Text>Naver Map</Text>
+          <View
+            style={{
+              width: Dimensions.get('window').width - 30,
+              height: 200,
+              marginTop: 10,
+            }}>
+            <NaverMapView
+              style={{width: '100%', height: '100%'}}
+              zoomControl={false}
+              center={{
+                zoom: 10,
+                tilt: 50,
+                latitude: (start.latitude + end.latitude) / 2,
+                longitude: (start.longitude + end.longitude) / 2,
+              }}>
+              <Marker
+                coordinate={{
+                  latitude: start.latitude,
+                  longitude: start.longitude,
+                }}
+                pinColor="blue"
+              />
+              <Path
+                coordinates={[
+                  {
+                    latitude: start.latitude,
+                    longitude: start.longitude,
+                  },
+                  {latitude: end.latitude, longitude: end.longitude},
+                ]}
+              />
+              <Marker
+                coordinate={{latitude: end.latitude, longitude: end.longitude}}
+              />
+            </NaverMapView>
           </View>
           <View style={styles.buttonWrapper}>
             <Pressable
