986a45b23d4890952a087edbc0b403b1b36c6001
ELLAERA <qwer2407@gmail.com>
2023-04-11 17:50:07 +0900

    installed Geolocation to get my location

--- src/pages/Ing.tsx
+++ src/pages/Ing.tsx
@@ -1,10 +1,134 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Dimensions, Text, View} from 'react-native';
+import NaverMapView, {Marker, Path} from 'react-native-nmap';
+import {useSelector} from 'react-redux';
+import {RootState} from '../store/reducer';
+import Geolocation from '@react-native-community/geolocation';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {LoggedInParamList} from '../../AppInner';
 
-function Ing() {
+type IngScreenProps = NativeStackScreenProps<LoggedInParamList, 'Delivery'>;
+
+function Ing({navigation}: IngScreenProps) {
+  console.dir(navigation);
+  const deliveries = useSelector((state: RootState) => state.order.deliveries);
+  const [myPosition, setMyPosition] = useState<{
+    latitude: number;
+    longitude: number;
+  } | null>(null);
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      info => {
+        setMyPosition({
+          latitude: info.coords.latitude,
+          longitude: info.coords.longitude,
+        });
+      },
+      console.error,
+      {
+        enableHighAccuracy: true,
+        timeout: 20000,
+      },
+    );
+  }, []);
+
+  if (!deliveries?.[0]) {
+    return (
+      <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
+        <Text>Please accept a order first!</Text>
+      </View>
+    );
+  }
+
+  if (!myPosition || !myPosition.latitude) {
+    return (
+      <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
+        <Text>
+          Loading my location. Please check if you have granted permission.
+        </Text>
+      </View>
+    );
+  }
+
+  const {start, end} = deliveries?.[0];
+
   return (
     <View>
-      <Text>Map</Text>
+      <View
+        style={{
+          width: Dimensions.get('window').width,
+          height: Dimensions.get('window').height,
+        }}>
+        <NaverMapView
+          style={{width: '100%', height: '100%'}}
+          zoomControl={false}
+          center={{
+            zoom: 10,
+            tilt: 50,
+            latitude: (start.latitude + end.latitude) / 2,
+            longitude: (start.longitude + end.longitude) / 2,
+          }}>
+          {myPosition?.latitude && (
+            <Marker
+              coordinate={{
+                latitude: myPosition.latitude,
+                longitude: myPosition.longitude,
+              }}
+              width={15}
+              height={15}
+              anchor={{x: 0.5, y: 0.5}}
+              caption={{text: 'Me'}}
+              image={require('../assets/red-dot.png')}
+            />
+          )}
+          {myPosition?.latitude && (
+            <Path
+              coordinates={[
+                {
+                  latitude: myPosition.latitude,
+                  longitude: myPosition.longitude,
+                },
+                {latitude: start.latitude, longitude: start.longitude},
+              ]}
+              color="orange"
+            />
+          )}
+          <Marker
+            coordinate={{
+              latitude: start.latitude,
+              longitude: start.longitude,
+            }}
+            width={15}
+            height={15}
+            anchor={{x: 0.5, y: 0.5}}
+            caption={{text: 'Depart'}}
+            image={require('../assets/blue-dot.png')}
+          />
+          <Path
+            coordinates={[
+              {
+                latitude: start.latitude,
+                longitude: start.longitude,
+              },
+              {latitude: end.latitude, longitude: end.longitude},
+            ]}
+            color="orange"
+          />
+          <Marker
+            coordinate={{latitude: end.latitude, longitude: end.longitude}}
+            width={15}
+            height={15}
+            anchor={{x: 0.5, y: 0.5}}
+            caption={{text: 'Arrived'}}
+            image={require('../assets/green-dot.png')}
+            onClick={() => {
+              console.log(navigation);
+              navigation.push('Complete', {orderId: deliveries[0].orderId});
+            }}
+          />
+        </NaverMapView>
+      </View>
     </View>
   );
 }
